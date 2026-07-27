c0a309c814f376e51dd9a47d1a5a7e5c8580cfe7
Subhradeep Sikder <isdsikder@gmail.com>
2026-07-27 08:27:33 +0530

    frontend init

---
+++ frontend/src/App.jsx
@@ -0,0 +1,14 @@
+
+function App() {
+
+
+  return (
+   <>
+   <div className="flex justify-center items-center h-screen text-2xl font-bold bg-amber-100">
+    <h1>Hello World</h1>
+   </div>
+   </>
+  )
+}
+
+export default App
